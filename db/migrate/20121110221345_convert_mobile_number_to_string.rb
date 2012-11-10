679879b371ceb68dc4830469753fdcb9c106b628
Brian Fountain <brian@brianfountain.com>
2012-11-10 17:15:02 -0500

    store mobile number as a string

---
+++ db/migrate/20121110221345_convert_mobile_number_to_string.rb
@@ -0,0 +1,5 @@
+class ConvertMobileNumberToString < ActiveRecord::Migration
+  def change
+    change_column :users, :mobile_number, :string
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121110191715) do
+ActiveRecord::Schema.define(:version => 20121110221345) do
 
   create_table "buildings", :force => true do |t|
     t.string   "name"
@@ -38,9 +38,10 @@ ActiveRecord::Schema.define(:version => 20121110191715) do
     t.integer  "unit_id"
     t.string   "first_name"
     t.string   "last_name"
-    t.integer  "mobile_number"
+    t.string   "mobile_number"
     t.datetime "created_at",    :null => false
     t.datetime "updated_at",    :null => false
+    t.boolean  "active"
   end
 
 end
