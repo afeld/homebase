b8e29fc77be729a56b6f7bb853578d183b62206f
Brian Fountain <brian@brianfountain.com>
2012-11-10 13:34:12 -0500

    added unsub keywords and removed verify authenticity token

--- app/controllers/tel_api_controller.rb
+++ app/controllers/tel_api_controller.rb
@@ -1,2 +1,7 @@
 class TelApiController < ApplicationController
+  skip_before_filter :verify_authenticity_token
+  
+  UNSUB_KEYWORDS = ['unsub', 'unsubscribe', 'quit', 'stop', 'Stop', 'Quit', 'Unsubscribe', 'Unsub', 'Unsubscribe.', 'Unsub.', 'Stop.', 'Quit.']
+  
+  
 end
