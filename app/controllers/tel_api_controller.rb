f5bcaff5069482640ce59caaa44939ff172a4b2e
Brian Fountain <brian@brianfountain.com>
2012-11-10 14:02:35 -0500

    added active to user model and stubbed in api methods

--- app/controllers/tel_api_controller.rb
+++ app/controllers/tel_api_controller.rb
@@ -2,6 +2,47 @@ class TelApiController < ApplicationController
   skip_before_filter :verify_authenticity_token
   
   UNSUB_KEYWORDS = ['unsub', 'unsubscribe', 'quit', 'stop', 'Stop', 'Quit', 'Unsubscribe', 'Unsub', 'Unsubscribe.', 'Unsub.', 'Stop.', 'Quit.']
+  HELP_TEXT = "help"
+  UNSUBSCRIBE_MESSAGE
   
+  def receive_text
+    message_body = params["Body"]
+    from_number = params["From"]
+    
+    user = User.find_or_create_by_phone_number(from_number)
+    unsubscribe = check_for_unsubscribe(message_body)
+    
+    #case 1 - User is unsubscribe
+    if user and unsubscribe
+      user.disable_user
+      text = UNSUBSCRIBE_MESSAGE
+    
+  end
+  
+  def message_unit
+    
+  end
+  
+  def message_user
+    
+  end
+  
+  def get_directory
+    
+  end
+  
+  def get_help
+    HELP_TEXT
+  end
+  
+  def check_for_unsubscribe(body)
+    unsubscribe = false
+    UNSUB_KEYWORDS.each do |kw|
+      if body == kw
+         unsubscribe = true
+      end
+    end
+    return unsubscribe
+  end
   
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,25 @@
 class User < ActiveRecord::Base
-  attr_accessible :first_name, :last_name, :mobile_number, :unit_id
+  attr_accessible :first_name, :last_name, :mobile_number, :unit_id, :active
   belongs_to :building
   has_many :units
+  
+  def self.active_users
+    find :all, :conditions => ['active = ?', true]
+  end
+  
+  def disable!
+    self.active = false
+    self.save
+  end
+  
+  def enable!
+    self.active = true
+    self.save
+  end
+  
+  def active?
+    self.active = true
+    self.save
+  end
+  
 end
